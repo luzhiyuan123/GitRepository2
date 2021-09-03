591e904ff740cccb067962bc17b11142710a1de1
luzhiyuan <lzy87654321@126.com>
2021-09-03 14:57:29 +0800

    第二次提交数据...

---
+++ TestModule01/src/com/msb/Student.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @Author: luzhiyuan
+ * @Date: 2021/9/3 09/03 13:35
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Student {
+    private int age;
+    private String name;
+}
